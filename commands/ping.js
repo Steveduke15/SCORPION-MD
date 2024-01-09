481f31e6958c6d674d77ad9550e4547694a628e6
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-01-09 17:47:23 +0500

    ●⟣⟤𝙆𝙄𝙉𝙂-𝙈𝘿/𝙑𝟭.𝟮⟥⟢●

--- commands/ping.js
+++ commands/ping.js
@@ -23,9 +23,9 @@ Secktor.cmd({
     },
     async(Void, citel) => {
         var inital = new Date().getTime();
-        const { key } = await Void.sendMessage(citel.chat, {text: '```Authorizing...```'});
+        const { key } = await Void.sendMessage(citel.chat, {text: '```ᴘɪɴɢ ᴋɪɴɢ-ᴍᴅ..```'});
         var final = new Date().getTime();
        // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*Lᴀᴛᴇɴᴄʏ*\n *' + (final - inital) + ' ms* ', edit: key});
+       return await Void.sendMessage(citel.chat, {text: '_𝙆𝙞𝙣𝙜 𝙥𝙤𝙣𝙜_\n *' + (final - inital) + ' ms* ', edit: key});
     }
 );
